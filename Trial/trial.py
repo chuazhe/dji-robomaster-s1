75640ffb492a4d6632211b8140898614ddd5d9a3
Chua Wei Zhe <chuazhe@gmail.com>
2025-04-24 16:21:24 +0800

    Update trial.py

--- Trial/trial.py
+++ Trial/trial.py
@@ -2,7 +2,7 @@ import time
 from robomaster import robot, blaster, led, chassis, gimbal
 
 red = 255
-green =0
+green = 0
 blue = 0
 
 x_val = 0.2
@@ -33,29 +33,57 @@ def robot_control(run):
         ep_chassis.move(x=0, y=-y_val, z=0, xy_speed=0.7).wait_for_completed()
         ep_chassis.move(x=0, y=y_val, z=0, xy_speed=0.7).wait_for_completed()
     elif run == ord('4'):
-        print("Shoot!")
-        ep_chassis.drive_speed(x=0, y=y_val, z=0, timeout=5)
-        time.sleep(1)
-        ep_blaster.set_led(brightness=32, effect=blaster.LED_ON)
-        time.sleep(1)
-        ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=5)
-        time.sleep(1)
-        ep_blaster.set_led(brightness=64, effect=blaster.LED_ON)
-        time.sleep(1)
-        ep_chassis.drive_speed(x=0, y=y_val, z=0, timeout=5)
-        time.sleep(1)
-        ep_blaster.set_led(brightness=128, effect=blaster.LED_ON)
-        time.sleep(1)
-        ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=5)
-        time.sleep(1)
-        ep_blaster.set_led(brightness=255, effect=blaster.LED_ON)
-        time.sleep(1)
-        ep_gimbal.recenter().wait_for_completed()
+        shoot()
+        # grip()
     elif run == ord('5'):
-        print("Dance!")
         dance()
 
+def shoot():
+    print("Shoot!")
+    ep_chassis.drive_speed(x=0, y=y_val, z=0, timeout=5)
+    time.sleep(1)
+    ep_blaster.set_led(brightness=32, effect=blaster.LED_ON)
+    time.sleep(1)
+    ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=5)
+    time.sleep(1)
+    ep_blaster.set_led(brightness=64, effect=blaster.LED_ON)
+    time.sleep(1)
+    ep_chassis.drive_speed(x=0, y=y_val, z=0, timeout=5)
+    time.sleep(1)
+    ep_blaster.set_led(brightness=128, effect=blaster.LED_ON)
+    time.sleep(1)
+    ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=5)
+    time.sleep(1)
+    ep_blaster.set_led(brightness=255, effect=blaster.LED_ON)
+    time.sleep(1)
+    ep_gimbal.recenter().wait_for_completed()
+
+def grip():
+    print("Grip!")
+    ep_chassis.drive_speed(x=0, y=y_val, z=0, timeout=5)
+    time.sleep(1)
+    ep_gripper.open(power=50)
+    time.sleep(1)
+    ep_gripper.pause()
+    ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=5)
+    time.sleep(1)
+    ep_arm.move(x=20, y=0).wait_for_completed()
+    ep_arm.move(x=0, y=20).wait_for_completed()
+    time.sleep(1)
+    ep_chassis.drive_speed(x=0, y=y_val, z=0, timeout=5)
+    time.sleep(1)
+    ep_arm.move(x=-20, y=0).wait_for_completed()
+    ep_arm.move(x=0, y=-20).wait_for_completed()
+    time.sleep(1)
+    ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=5)
+    time.sleep(1)
+    ep_gripper.close(power=50)
+    time.sleep(1)
+    ep_gripper.pause()
+    ep_gimbal.recenter().wait_for_completed()
+
 def dance():
+    print("Dance!")
     ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=5)
     time.sleep(1)
     chassis_action = ep_chassis.drive_speed(x=0, y=0, z=-z_val, timeout=5)
@@ -100,5 +128,5 @@ if __name__ == '__main__':
     ep_blaster = ep_robot.blaster
     ep_gimbal = ep_robot.gimbal
 
-    # ep_robot.set_robot_mode(mode=robot.CHASSIS_LEAD)
+    ep_robot.set_robot_mode(mode=robot.CHASSIS_LEAD)
     ep_robot.sub_game_msg(callback=sub_data_handler)
