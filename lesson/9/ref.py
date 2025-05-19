e2f045d169d55b59bd09e8caea7f96b53f92875f
Chua Wei Zhe <chuazhe@gmail.com>
2025-05-19 18:32:08 +0800

    Update ref.py

--- lesson/9/ref.py
+++ lesson/9/ref.py
@@ -1,4 +1,4 @@
-# Slide 
+# Slide 23_1
 #导入编程操控机器人需要的模块
 from robomaster import robot
 from robomaster import led
@@ -20,7 +20,7 @@ ep_robot.gimbal.move(pitch=0, yaw=-100, pitch_speed=100, yaw_speed=100).wait_for
 ###结束机器人程序###
 ep_robot.close()
 
-# Slide 
+# Slide 23_2
 from robomaster import robot
 from robomaster import led
 from robomaster import blaster
@@ -43,7 +43,63 @@ ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
 ep_robot.close()
 
 
-# Slide 
+
+# Slide 26_1
+from robomaster import robot
+from robomaster import led
+from robomaster import blaster
+import time
+
+###机器人初始化###
+#在程序内实例化Robot对象，相当于在程序内部有一个EP机器人
+#程序与机器人联网，“ap”表示直连模式连接；“sta”表示路由器模式连接；rndis表示使用USB连接
+ep_robot = robot.Robot()
+ep_robot.initialize(conn_type="sta")
+
+###你要写的程序###
+ep_robot.gimbal.moveto(pitch=0, yaw=50, pitch_speed=100, yaw_speed=100).wait_for_completed()
+ep_robot.gimbal.moveto(pitch=0, yaw=-50, pitch_speed=100, yaw_speed=100).wait_for_completed()
+
+
+###结束机器人程序###
+ep_robot.close()
+
+
+
+# Slide 26_2
+from robomaster import robot
+from robomaster import led
+from robomaster import blaster
+import time
+
+###机器人初始化###
+#在程序内实例化Robot对象，相当于在程序内部有一个EP机器人
+#程序与机器人联网，“ap”表示直连模式连接；“sta”表示路由器模式连接；rndis表示使用USB连接
+ep_robot = robot.Robot()
+ep_robot.initialize(conn_type="sta")
+
+###你要写的程序###
+ep_robot.gimbal.moveto(pitch=0, yaw=30, pitch_speed=100, yaw_speed=100).wait_for_completed()
+ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
+time.sleep(1)
+ep_robot.gimbal.moveto(pitch=0, yaw=60, pitch_speed=100, yaw_speed=100).wait_for_completed()
+ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
+time.sleep(1)
+ep_robot.gimbal.moveto(pitch=0, yaw=-30, pitch_speed=100, yaw_speed=100).wait_for_completed()
+ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
+time.sleep(1)
+ep_robot.gimbal.moveto(pitch=0, yaw=-60, pitch_speed=100, yaw_speed=100).wait_for_completed()
+ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
+time.sleep(1)
+ep_robot.gimbal.moveto(pitch=0, yaw=0, pitch_speed=100, yaw_speed=100).wait_for_completed()
+ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
+
+
+###结束机器人程序###
+ep_robot.close()
+
+
+# Slide 27
 from robomaster import robot
 from robomaster import led
 from robomaster import blaster
@@ -81,102 +137,7 @@ ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
 ep_robot.close()
 
 
-# Slide 
-from robomaster import robot
-from robomaster import led
-from robomaster import blaster
-import time
-
-###机器人初始化###
-#在程序内实例化Robot对象，相当于在程序内部有一个EP机器人
-#程序与机器人联网，“ap”表示直连模式连接；“sta”表示路由器模式连接；rndis表示使用USB连接
-ep_robot = robot.Robot()
-ep_robot.initialize(conn_type="sta")
-
-###你要写的程序###
-ep_robot.gimbal.moveto(pitch=0, yaw=50, pitch_speed=100, yaw_speed=100).wait_for_completed()
-ep_robot.gimbal.moveto(pitch=0, yaw=-50, pitch_speed=100, yaw_speed=100).wait_for_completed()
-
-
-###结束机器人程序###
-ep_robot.close()
-
-
-
-# Slide 
-from robomaster import robot
-from robomaster import led
-from robomaster import blaster
-import time
-
-###机器人初始化###
-#在程序内实例化Robot对象，相当于在程序内部有一个EP机器人
-#程序与机器人联网，“ap”表示直连模式连接；“sta”表示路由器模式连接；rndis表示使用USB连接
-ep_robot = robot.Robot()
-ep_robot.initialize(conn_type="sta")
-
-###你要写的程序###
-ep_robot.gimbal.moveto(pitch=0, yaw=30, pitch_speed=100, yaw_speed=100).wait_for_completed()
-ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
-time.sleep(1)
-ep_robot.gimbal.moveto(pitch=0, yaw=60, pitch_speed=100, yaw_speed=100).wait_for_completed()
-ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
-time.sleep(1)
-ep_robot.gimbal.moveto(pitch=0, yaw=-30, pitch_speed=100, yaw_speed=100).wait_for_completed()
-ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
-time.sleep(1)
-ep_robot.gimbal.moveto(pitch=0, yaw=-60, pitch_speed=100, yaw_speed=100).wait_for_completed()
-ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
-time.sleep(1)
-ep_robot.gimbal.moveto(pitch=0, yaw=0, pitch_speed=100, yaw_speed=100).wait_for_completed()
-ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE, times=1)
-
-
-###结束机器人程序###
-ep_robot.close()
-
-
-# Slide 
-from robomaster import robot
-from robomaster import led
-from robomaster import blaster
-import time
-
-###机器人初始化###
-#在程序内实例化Robot对象，相当于在程序内部有一个EP机器人
-#程序与机器人联网，“ap”表示直连模式连接；“sta”表示路由器模式连接；rndis表示使用USB连接
-ep_robot = robot.Robot()
-ep_robot.initialize(conn_type="sta")
-
-###你要写的程序###
-ep_robot.gimbal.moveto(pitch=-5, yaw=45, pitch_speed=100, yaw_speed=100).wait_for_completed()
-ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE,times=1)
-time.sleep(1)
-ep_robot.gimbal.moveto(pitch=-5, yaw=0, pitch_speed=100, yaw_speed=100).wait_for_completed()
-
-ep_robot.chassis.move(x=0.8, y=0, z=0, xy_speed=0.7).wait_for_completed()
-ep_robot.gimbal.moveto(pitch=-5, yaw=-30, pitch_speed=100, yaw_speed=100).wait_for_completed()
-ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE,times=1)
-time.sleep(1)
-ep_robot.gimbal.moveto(pitch=-5, yaw=0, pitch_speed=100, yaw_speed=100).wait_for_completed()
-
-ep_robot.chassis.move(x=1.4, y=0, z=0, xy_speed=0.7).wait_for_completed()
-ep_robot.gimbal.moveto(pitch=10, yaw=70, pitch_speed=100, yaw_speed=100).wait_for_completed()
-ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE,times=1)
-time.sleep(1)
-ep_robot.gimbal.moveto(pitch=-5, yaw=0, pitch_speed=100, yaw_speed=100).wait_for_completed()
-
-ep_robot.chassis.move(x=0.3, y=0, z=0, xy_speed=0.7).wait_for_completed()
-ep_robot.blaster.fire(fire_type=blaster.WATER_FIRE,times=1)
-time.sleep(1)
-ep_robot.gimbal.moveto(pitch=0, yaw=0, pitch_speed=100, yaw_speed=100).wait_for_completed()
-
-
-###结束机器人程序###
-ep_robot.close()
-
-
-# Slide 
+# Slide 28
 from robomaster import robot
 from robomaster import led
 from robomaster import blaster
@@ -204,7 +165,7 @@ ep_robot.gimbal.drive_speed(pitch_speed=0, yaw_speed=0)
 ep_robot.close()
 
 
-# Slide 
+# Slide 27
 from robomaster import robot
 from robomaster import led
 from robomaster import blaster
@@ -226,7 +187,7 @@ ep_robot.gimbal.recenter(pitch_speed=0, yaw_speed=100)
 ep_robot.close()
 
 
-# Slide 
+# Maybe no used 
 from robomaster import robot
 from robomaster import led
 from robomaster import blaster
