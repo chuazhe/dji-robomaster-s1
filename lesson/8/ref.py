2ddf6b4f16086e702fa44313e25dcc82aef6e0e8
Chua Wei Zhe <chuazhe@gmail.com>
2025-06-11 14:23:55 +0800

    Update ref.py

--- lesson/8/ref.py
+++ lesson/8/ref.py
@@ -166,19 +166,3 @@ time.sleep(10)
 
 ###结束机器人程序###
 ep_robot.close()#导入编程操控机器人需要的模块
-import robomaster
-from robomaster import robot
-from robomaster import led
-import time
-
-###机器人初始化###
-#在程序内实例化Robot对象，相当于在程序内部有一个EP机器人
-#程序与机器人联网，“ap”表示直连模式连接；“sta”表示路由器模式连接；rndis表示使用USB连接
-ep_robot = robot.Robot()
-ep_robot.initialize(conn_type="sta")
-
-###你要写的程序###
-
-
-###结束机器人程序###
-ep_robot.close()
